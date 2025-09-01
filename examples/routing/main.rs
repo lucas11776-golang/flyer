8f9c8fc069761cce7a22f58087a4c32a1d556ed2
lucas11776 <lucas11776>
2025-09-01 11:17:46 +0200

    Updated: working on docs before adding new feature

---
+++ examples/routing/main.rs
@@ -0,0 +1,17 @@
+use std::{io::Result};
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut server = flyer::server("127.0.0.1", 9999).await?;
+    
+    server.router().get("", |req, res| {
+        return res
+    }, None);
+
+    print!("\r\n\r\nRunning server: {}\r\n\r\n", server.address());
+
+    server.listen().await;
+
+    Ok(())
+}
