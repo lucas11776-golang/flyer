fae4bddcf690e7455a43f85e081c2ddcfd86c81d
lucas11776 <lucas11776>
2025-10-30 16:31:18 +0200

    Updated: package deps crates version updated

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::{fs::File, io::Write};
-
-use flyer::{view::view_data};
 use serde::{Deserialize, Serialize};
 
-// TODO: take rest must make controller route to support async operation...
+use flyer::ws::Event;
 
 #[derive(Serialize, Deserialize)]
 pub struct User<'a> {
@@ -17,20 +14,18 @@ fn main() {
     let mut server = flyer::server_tls("127.0.0.1", 9999, "host.key", "host.cert")
         .view("views");
 
-
     server.router().ws("/", async |req, mut ws| {
         ws.on(async |event, mut writer| {
             match event {
-                flyer::ws::Event::Ready() => writer.write("Ready".as_bytes().to_vec()).await,
-                flyer::ws::Event::Message(items) => writer.write("Message".as_bytes().to_vec()).await,
-                flyer::ws::Event::Ping(items) => writer.write("Ping".as_bytes().to_vec()).await,
-                flyer::ws::Event::Pong(items) => writer.write("Pong".as_bytes().to_vec()).await,
-                flyer::ws::Event::Close(reason) => todo!(),
+                Event::Ready() => writer.write("Ready".as_bytes().to_vec()).await,
+                Event::Message(items) => writer.write("Message".as_bytes().to_vec()).await,
+                Event::Ping(items) => writer.write("Ping".as_bytes().to_vec()).await,
+                Event::Pong(items) => writer.write("Pong".as_bytes().to_vec()).await,
+                Event::Close(reason) => todo!(),
             }
         });
 
     }, None);
-
 
     print!("\r\n\r\nRunning server: {}\r\n\r\n", server.address());
 
